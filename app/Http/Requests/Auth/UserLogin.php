b02bbed5d5c392b94bc25be4009ac78d6d67e556
Masiur Rahman Siddiki <mrsiddiki@gmail.com>
2021-01-10 11:47:28 +0600

    logout button done

--- app/Http/Requests/Auth/UserLogin.php
+++ app/Http/Requests/Auth/UserLogin.php
@@ -24,7 +24,7 @@ class UserLogin extends FormRequest
     public function rules()
     {
         return [
-            'email' => 'required|strinUg|email',
+            'email' => 'required|string|email',
             'password' => 'required|string',
         ];
     }
