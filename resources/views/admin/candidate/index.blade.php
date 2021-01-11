b165651d2f992271ddb252e87af6c155ba4de159
Masiur Rahman Siddiki <mrsiddiki@gmail.com>
2021-01-11 11:31:32 +0600

    updated

--- resources/views/admin/candidate/index.blade.php
+++ resources/views/admin/candidate/index.blade.php
@@ -9,7 +9,7 @@
                 </div><!-- /.col -->
                 <div class="col-sm-6">
                     <ol class="breadcrumb float-sm-right">
-                        <li class="breadcrumb-item"><a href="#">Home</a></li>
+                        <li class="breadcrumb-item"><a href="{{ route('dashboard') }}">Dashboard</a></li>
                         <li class="breadcrumb-item active"><a href="#">Candidates</a></li>
 
                     </ol>
@@ -23,7 +23,8 @@
             <div class="col-12">
                 <div class="card">
                     <div class="card-header">
-                        <h3 class="card-title">DataTable with minimal features & hover style</h3>
+{{--                        <h3 class="card-title">DataTable with minimal features & hover style</h3>--}}
+                        <a class="card-tools btn btn-info btn-sm" href="{{ route('candidate.create') }}">Add New Candidate</a>
                     </div>
                     <!-- /.card-header -->
                     <div class="card-body">
