2e6213d0125ba62324e4a4549c26d6543c754d28
Masiur Rahman Siddiki <mrsiddiki@gmail.com>
2021-01-11 12:48:33 +0600

    updated dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,10 +3,17 @@
 namespace App\Http\Controllers;
 
 
+use App\Models\Candidate;
+use App\Models\Seat;
+
 class DashboardController extends Controller
 {
     public function dashboardMain()
     {
-        return view('dashboard');
+         $countSeat = Seat::count();
+          $countCandidate = Candidate::groupBy('name')->count();
+        return view('dashboard')
+            ->with('number_of_seat', $countSeat)
+            ->with('number_of_unique_candidate', $countCandidate);
     }
 }
