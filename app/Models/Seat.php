bc17ea11035eabb5a58799fc7868a42bf5f8ee74
Masiur Rahman Siddiki <mrsiddiki@gmail.com>
2021-01-12 11:02:41 +0600

    updated

--- app/Models/Seat.php
+++ app/Models/Seat.php
@@ -2,14 +2,26 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Seat extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     protected $guarded = ['id'];
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        // Order by name ASC
+        static::addGlobalScope('order', function (Builder $builder) {
+            $builder->orderBy('priority', 'asc');
+        });
+    }
 
     public function candidate()
     {
