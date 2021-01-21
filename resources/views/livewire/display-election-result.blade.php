4ffd170bc6b853fd5e5da6565386b966df6c0e77
Masiur Rahman Siddiki <mrsiddiki@gmail.com>
2021-01-21 15:15:46 +0600

    live page bug fixed

--- resources/views/livewire/display-election-result.blade.php
+++ resources/views/livewire/display-election-result.blade.php
@@ -4,18 +4,18 @@
         <div class="row">
             <div class="col-md-6">
                 @foreach($seats as $seat)
-                    @unless($seat->name == 'কার্যনির্বাহী সদস্য')
+                    @unless($seat->name == 'President')
                         <div>
-                            <h4> {{ $seat->name }}</h4>
-                            @if($seat->name == 'সভাপতি')
+                            <h4> {{ $seat->name_bn }}</h4>
+                            @if($seat->name == 'President')
                                 <table class="table-warning table">
-                            @elseif($seat->name == 'সহ-সভাপতি')
+                            @elseif($seat->name == 'Vice President')
                                 <table class="table-primary table">
-                            @elseif($seat->name == 'কোষাধ্যক্ষ')
+                            @elseif($seat->name == 'Treasurer')
                                 <table class="table-info table">
-                            @elseif($seat->name == 'সাধারণ সম্পাদক')
+                            @elseif($seat->name == 'General Secretary')
                                 <table class="table-success table">
-                            @elseif($seat->name == 'সহ-সাধারণ সম্পাদক')
+                            @elseif($seat->name == 'Assistant General Secretary')
                                 <table class="table-primary table">
                             @endif
                                 @foreach(collect($seat->candidates->where('year_id', $year->id))->sortByDesc('number_of_votes') as $candidate)
@@ -36,9 +36,9 @@
 
             <div class="col-md-6">
                 @foreach($seats as $seat)
-                    @if($seat->name == 'কার্যনির্বাহী সদস্য')
+                    @if($seat->name == 'Executive Member')
                         <div>
-                            <h4> {{ $seat->name }}</h4>
+                            <h4> {{ $seat->name_bn }}</h4>
                             <table class="table-active table">
                                 @foreach(collect($seat->candidates->where('year_id', $year->id))->sortByDesc('number_of_votes') as $candidate)
                                     <tr>
