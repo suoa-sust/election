be15a4bcff6a427d64f4d61e3afbbf18fd2545db
Masiur Rahman Siddiki <mrsiddiki@gmail.com>
2021-01-26 17:03:24 +0600

    no live page updated

--- resources/views/nolive.blade.php
+++ resources/views/nolive.blade.php
@@ -5,6 +5,8 @@
         <h1>No LIVE event running now</h1>
 {{--        <h3>শাহজালাল ইউনিভার্সিটি অফিসার এসোসিয়েশন নির্বাচন ২০২১</h3>--}}
 {{--        <img src="{{ asset('live.gif') }}" width="70px"><livewire:current-time />--}}
+
+        <a class="btn btn-success" href="{{ route('home') }}">Go Back To Home</a>
     </div>
 
     <br>
