28acd3c218b1bd36a59c20d07b519cf6d32d21de
Masiur Rahman Siddiki <mrsiddiki@gmail.com>
2021-01-28 12:35:50 +0600

    updated

--- resources/views/livewire/display-election-result.blade.php
+++ resources/views/livewire/display-election-result.blade.php
@@ -1,4 +1,4 @@
-<div wire:poll>
+<div wire:poll.keep>
 
     <div class="container">
         <div class="row">
@@ -6,22 +6,27 @@
                 @foreach($seats as $seat)
                     @unless($seat->name == 'Executive Member')
                         <div>
-                            <h4> {{ $seat->name_bn }}</h4>
+
                             @if($seat->name == 'President')
+                                <h4 style="font-weight: bold;"> {{ $seat->name_bn }}</h4>
                                 <table class="table-warning table">
                             @elseif($seat->name == 'Vice President')
+                                <h4> {{ $seat->name_bn }}</h4>
                                 <table class="table-primary table">
                             @elseif($seat->name == 'Treasurer')
+                                <h4> {{ $seat->name_bn }}</h4>
                                 <table class="table-info table">
                             @elseif($seat->name == 'General Secretary')
+                                <h4 style="font-weight: bold;"> {{ $seat->name_bn }}</h4>
                                 <table class="table-success table">
                             @elseif($seat->name == 'Assistant General Secretary')
+                                <h4> {{ $seat->name_bn }}</h4>
                                 <table class="table-primary table">
                             @endif
                                 @foreach(collect($seat->candidates->where('year_id', $year->id))->sortByDesc('number_of_votes') as $candidate)
                                     <tr>
-                                        <td>{{ $candidate->name }}</td>
-                                        <td>{{ $candidate->designation }}</td>
+                                        <td >{{ $candidate->name }}</td>
+                                        <td >{{ $candidate->designation }}</td>
                                         <td style="font-size: medium"><strong>{{ $candidate->number_of_votes  }}</strong></td>
                                     </tr>
                                 @endforeach
