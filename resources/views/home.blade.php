33d7791e81c089646123e5a7a4941c118788db78
Masud <asraf.sustcse@gmail.com>
2022-03-20 15:51:39 +0600

    date and time update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,7 +12,7 @@
 {{--            <h4> ভোটগ্রহণের সময়: <span style="color: #904201"><strong>সকাল ১০ টা থেকে বিকাল ৪ টা পর্যন্ত</strong></span></h4>--}}
 
             <h4>সর্বশেষ নির্বাচনের তারিখ: <span style="color: #904201"><strong>31 জানুয়ারি, 2021 রোজঃ রবিবার</strong></span></h4>
-            <h4> ভোটগ্রহণের সময়: <span style="color: #904201"><strong>সকাল ১০ টা থেকে বিকাল ৪ টা পর্যন্ত</strong></span></h4>
+            <h4> ভোটগ্রহণের সময়: <span style="color: #904201"><strong>সকাল ১০ টা থেকে বিকাল ৩.৩০ টা পর্যন্ত</strong></span></h4>
 
 
             <br><br>
@@ -45,7 +45,7 @@
 
             <h4> পদসংখ্যাঃ <span style="color: #065632"><strong>১১ টি</strong></span></h4>
             <h4> মোট প্রার্থী: <span style="color: #065632"><strong>২২ জন</strong></span></h4>
-            <h4> মোট ভোটার: <span style="color: #065632"><strong>২৫৮ জন</strong></span></h4>
+            <h4> মোট ভোটার: <span style="color: #065632"><strong>২৬৪ জন</strong></span></h4>
 
 
 
