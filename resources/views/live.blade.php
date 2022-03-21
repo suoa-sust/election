efc42aa6788996939a622463ac091869830532fa
Masud <asraf.sustcse@gmail.com>
2022-03-21 14:43:46 +0600

    year and logo update

--- resources/views/live.blade.php
+++ resources/views/live.blade.php
@@ -1,8 +1,9 @@
 @extends('layouts.live')
 @section('content')
     <div class="text-center">
-        <img src="https://www.sust.edu/images/logo.png" width="80px">
-        <h3>শাহজালাল বিশ্ববিদ্যালয় অফিসার্স এসোসিয়েশন নির্বাচন ২০২১</h3>
+        <img src="{{ asset('dist/img/sust_logo.png') }}" width="80px">
+{{--        <img src="{{ asset('dist/img/suoa_logo_v.svg') }}" width="80px">--}}
+        <h3>শাহজালাল বিশ্ববিদ্যালয় অফিসার্স এসোসিয়েশন নির্বাচন ২০২২</h3>
 
         <livewire:current-time /><img src="{{ asset('live.gif') }}" width="70px" style="padding-bottom: 6px;">
     </div>
