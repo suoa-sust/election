cfa84d0a21d2a6a6583f6fecf6a1d8e2e7da18c3
Masud <asraf.sustcse@gmail.com>
2023-01-31 17:04:44 +0600

    update

--- resources/views/live.blade.php
+++ resources/views/live.blade.php
@@ -3,7 +3,7 @@
     <div class="text-center">
 {{--        <img src="{{ asset('dist/img/sust_logo.png') }}" width="80px">--}}
 {{--        <img src="{{ asset('dist/img/suoa_logo_v.svg') }}" width="80px">--}}
-        <h3 class="pt-2">শাহজালাল বিশ্ববিদ্যালয় অফিসার্স এসোসিয়েশন নির্বাচন ২০২২</h3>
+        <h3 class="pt-2">শাহজালাল বিশ্ববিদ্যালয় অফিসার্স এসোসিয়েশন নির্বাচন ২০২৩</h3>
 
         <livewire:current-time /><img src="{{ asset('live.gif') }}" width="70px" style="padding-bottom: 6px;">
     </div>
